b613f2c8fa7207c1975ef79e19517fbca01a6dec
Tugrul Ates <me@tugrulates.com>
2024-12-17 22:38:08 +0100

    Cleanup unused code (#6)

--- common/readme.ts
+++ common/readme.ts
@@ -4,13 +4,13 @@ import { join } from "jsr:@std/path";
 /**
  * Print out a README file from a command and its subcommands recursively.
  */
-function logReadme(command: Command, depth = 1) {
+function logReadme(command: Command, { depth = 1 } = {}) {
   console.log(`${"#".repeat(depth)} ${command.getName()}`);
   console.log();
   console.log("```" + command.getHelp({ colors: false }) + "```");
   for (const subCommand of command.getCommands()) {
     console.log();
-    logReadme(subCommand, depth + 1);
+    logReadme(subCommand, { depth: depth + 1 });
   }
 }
 
